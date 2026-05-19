5eb2269de23d6efd08c54154f978d10911ae7b26
w-danieldevs <wdsaavedra408@gmail.com>
2026-05-19 11:37:58 -0500

    mi segundo commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,45 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Cuadrado } from './cuadrado/cuadrado';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Cuadrado],
+  imports: [Cuadrado],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
-  protected readonly title = signal('mi');
-  coloractual = signal('Red');
+  protected readonly title = signal('');
+  coloractual = signal('white');
+
+   nombredelcolor() {
+
+    if (this.coloractual() == 'white') {
+      return 'blanco';
+    }
+
+    if (this.coloractual() == 'Red') {
+      return 'rojo';
+    }
+
+    if (this.coloractual() == 'Blue') {
+      return 'azul';
+    }
+
+    if (this.coloractual() == 'Yellow') {
+      return 'amarillo';
+    }
+
+    if (this.coloractual() == 'Green') {
+      return 'verde';
+    }
+
+    return this.coloractual();
+  }
+
 
   actualizarcolor(color: string) {
     this.coloractual.set(color);
   }
 }
+
